84038ece2e22c8ccfbd41236ae3f3b47727c983e
memayank2007 <memayank2007@gmail.com>
2017-03-26 21:15:08 +0530

    added routing to the page

---
+++ client/components/NavigationBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default () => {
+  return(
+    <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <a className="navbar-brand"  href ="#"> Red Dice </a>
+        </div>
+
+        <div className="collapse navbar-collapse">
+          <ul className="nav navbar-bar navbar-right">
+            <li> <a href="#"> Sign Up </a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
